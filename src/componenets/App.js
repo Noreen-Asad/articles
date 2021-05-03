f9f2d17b57f4151f2bf39cf1364949e7f5a761dc
Noreen Asad <noreenassad@gmail.com>
2021-05-03 15:49:56 +0100

    use hooks

--- src/componenets/App.js
+++ src/componenets/App.js
@@ -1,30 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import unsplash from "../api/unsplash";
 import ArticleList from "./ArticleList";
 
-class App extends React.Component {
-  state = {
-    articles: [],
-  };
+function App() {
 
-  async componentDidMount() {
-    const response = await unsplash.get("photos", {
+  const [articles, setArticles] = useState([])
+
+  useEffect(() => {
+    unsplash.get("photos", {
       params: { per_page: "15" },
-    });
-    console.log(response);
-    this.setState({ articles: response.data });
-  }
+    }).then(data => setArticles(data.data)).catch(err => alert(err))
+  }, [setArticles, articles]);
 
-  render() {
-    if (this.state.articles) {
-      return (
-        <div className="ui container" style={{ marginTop: "10px" }}>
-          <ArticleList articles={this.state.articles} />
-        </div>
-      );
-    }
-    return null;
+  if (articles) {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <ArticleList articles={articles} />
+      </div>
+    );
   }
+  return null;
 }
 
 export default App;
