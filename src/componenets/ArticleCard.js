3471ea324ada0933d89bdb7df6e99f0d29127b32
Noreen Asad <noreenassad@gmail.com>
2021-05-04 09:42:25 +0100

    clean up

--- src/componenets/ArticleCard.js
+++ src/componenets/ArticleCard.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import "./ArticleList.scss";
 
-const ArticleCard = ({urls, description, alt_description}) => {
-  console.log(description)
+const ArticleCard = ({ urls, description, alt_description }) => {
   const [details, setDescription] = useState(description);
   const [lightboxDisplay, setLightboxDisplay] = useState(false);
   const showLightBox = () => {
@@ -12,32 +11,32 @@ const ArticleCard = ({urls, description, alt_description}) => {
   const hideLightBox = () => {
     setLightboxDisplay(false);
   };
-    let overview;
-    if (alt_description) {
-     overview = <p>{alt_description}</p>
-    }
-    else{
-      overview = <p>no description available</p>
-    }
-    return (
-      <div className="card">
-        <div
-          onClick={() => {showLightBox(urls['regular'], details)}}
-          className="article-img"
-        >
-          <img alt={description} src={urls['regular']} />
-        </div>
-        <div>{description}</div>
-        <div>{alt_description}</div>
-        {lightboxDisplay ? (
-          <div className="lightbox" onClick={hideLightBox}>
-            {overview}
-          </div>
-        ) : (
-          ""
-        )}
+  let overview;
+  if (alt_description) {
+    overview = <p>{alt_description}</p>
+  }
+  else {
+    overview = <p>no description available</p>
+  }
+  return (
+    <div className="card">
+      <div
+        onClick={() => { showLightBox(urls['regular'], details) }}
+        className="article-img"
+      >
+        <img alt={description} src={urls['regular']} />
       </div>
-    );
+      <div>{description}</div>
+      <div>{alt_description}</div>
+      {lightboxDisplay ? (
+        <div className="lightbox" onClick={hideLightBox}>
+          {overview}
+        </div>
+      ) : (
+        ""
+      )}
+    </div>
+  );
 }
 
 export default ArticleCard;
